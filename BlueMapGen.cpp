b0082e6d64d9b4075d73deefde1d90c326d83a9e
Kysumi <maunderscott@gmail.com>
2020-06-03 20:54:37 +1200

    Moving to ChakraCore because of lack of v8 documentation

--- BlueMapGen.cpp
+++ BlueMapGen.cpp
@@ -1,17 +1,80 @@
-#include "V8Wrapper.h"
+#include "ChakraCore.h"
+#include <string>
+#include <iostream>
+#include <queue>
 
-int main(int argc, char* argv[]) {
-    
-    V8Wrapper v8;
+#include "Helper.h"
 
-    v8.startV8(argv[0]);
+void CALLBACK PromiseContinuationCallback(JsValueRef task, void* callbackState)
+{
+    // Save promise task in taskQueue.
+    std::queue<JsValueRef>* q = (std::queue<JsValueRef>*)callbackState;
+    q->push(task);
+    JsAddRef(task, nullptr);
+}
 
+void runPromiseSample()
+{
+	std::queue<JsValueRef> taskQueue;
+    JsValueRef result;
+    JsSetPromiseContinuationCallback(PromiseContinuationCallback, &taskQueue);
+    JsRunScript(
+        L"//The JavaScript ES6 Promise code goes here\n" \
+        L"new Promise((resolve, reject) => resolve('basic:success'))" \
+        L".then(() => {return 'second:success'});",
+        JS_SOURCE_CONTEXT_NONE, L"", &result);
 
-	// Doesn't work from here for some reason
-    // v8.runScript("test.js");
-    
-    v8.shutdownV8();
+    JsValueRef global;
+    JsGetGlobalObject(&global);
 
+    // Execute promise tasks stored in taskQueue
+    while (!taskQueue.empty()) {
+        JsValueRef task = taskQueue.front();
+        taskQueue.pop();
+        JsCallFunction(task, &global, 1, &result);
+        JsRelease(task, nullptr);
+    }
+}
+
+int main()
+{
+    JsRuntimeHandle runtime;
+    JsContextRef context;
+    JsValueRef result;
+    unsigned currentSourceContext = 0;
+
+    // Your script; try replace hello-world with something else
+    std::wstring script = Helper::getFileContents("tesast.js");
+
+    // Create a runtime. 
+    JsCreateRuntime(JsRuntimeAttributeNone, nullptr, &runtime);
+
+    // Create an execution context. 
+    JsCreateContext(runtime, &context);
+
+    // Now set the current execution context.
+    JsSetCurrentContext(context);
+
+    // Run the script.
+    JsRunScript(script.c_str(), currentSourceContext++, L"", &result);
+
+    // Convert your script result to String in JavaScript; redundant if your script returns a String
+    JsValueRef resultJSString;
+    JsConvertValueToString(result, &resultJSString);
+
+    // Project script result back to C++.
+    const wchar_t* resultWC;
+    size_t stringLength;
+    JsStringToPointer(resultJSString, &resultWC, &stringLength);
+
+    std::wstring resultW(resultWC);
+    std::cout << std::string(resultW.begin(), resultW.end()) << std::endl;
+
+    // Dispose runtime
+    JsSetCurrentContext(JS_INVALID_REFERENCE);
+    JsDisposeRuntime(runtime);
+
+    runPromiseSample();
 
     return 0;
 }
--- Helper.h
+++ Helper.h
@@ -1,29 +1,24 @@
 #pragma once
-#include <libplatform/libplatform.h>
-#include <assert.h>
-#include <v8.h>
+#include <codecvt>
+#include <iostream>
+#include <fstream>
 
 class Helper
 {
 public:
-    static std::string getFileContents(const char* filename)
+    static std::wstring getFileContents(const char* fileName)
     {
-        std::ifstream in(filename, std::ios::in | std::ios::binary);
+        std::ifstream in(fileName, std::ios::in | std::ios::binary);
         if (in)
         {
-            return(std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>()));
+	        const auto string = std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+
+            return std::wstring_convert<std::codecvt_utf8<wchar_t>>().from_bytes(string);
         }
-        throw(errno);
-    }
 
-    static const char* ToCString(const v8::String::Utf8Value& value) {
-        return *value ? *value : "<string conversion failed>";
-    }
-
-    static v8::MaybeLocal<v8::String> ReadFile(v8::Isolate* isolate, const char* name) {
-        v8::MaybeLocal<v8::String> result = v8::String::NewFromUtf8(
-            isolate, Helper::getFileContents("test.js").c_str(), v8::NewStringType::kNormal);
-        return result;
+        std::cout << "Could not load file: " << fileName << std::endl;
+    	
+        return L"";
     }
 };
 

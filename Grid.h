74b76424ce6822bf8715d42d08f533d3e09e178f
Maunder <scott.maunder@ezyvet.com>
2020-06-09 23:26:11 +1200

    Bindings might not work but they complie

--- Grid.h
+++ Grid.h
@@ -3,21 +3,31 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include "Node.h"
+#include <ChakraCommon.h>
 
 class Grid {
 public:
 	Grid(sf::Vector2i size);
 	~Grid();
+
 	void Process();
 	void Draw(sf::RenderWindow& window);
+	void ProjectClass();
 
+	void BindToJS();
+
+	std::vector<Node> getNeighbours(sf::Vector2i position);
+	std::vector<Node> getAliveNeighbours(sf::Vector2i position);
+	std::vector<Node> getNeighbours(int x, int y);
+	
+	static JsValueRef JSGridPrototype;
+	
 private:
 	const int nodeSize = 12;
 	sf::Vector2i size;
 
 	std::vector<std::vector<Node>> grid;
-	
-	std::vector<Node> getNeighbours(int x, int y);
-	std::vector<Node> getNeighbours(sf::Vector2i position);
-	std::vector<Node> getAliveNeighbours(sf::Vector2i position);
+
+	std::vector<Node> getNode(int x, int y);
+	std::vector<Node> getNode(sf::Vector2i position);
 };

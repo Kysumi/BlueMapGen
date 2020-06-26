5bbcfd2b66bac2fc4b4d5d3eed46efee59655303
Maunder <maunderscott@gmail.com>
2020-06-26 17:30:31 +1200

    Build working again

--- src/ChakraCore/Chakra.h
+++ src/ChakraCore/Chakra.h
@@ -5,10 +5,17 @@ public:
     static bool runScripty();
 
 private:
-
+    static HRESULT CreateRuntime(JsRuntimeHandle *runtime);
+    static HRESULT CreateParserStateAndRunScript(const char* fileName, LPCSTR fileContents, size_t fileLength, JsFinalizeCallback fileContentsFinalizeCallback, char *fullPath);
+    static HRESULT GetSerializedBuffer(LPCSTR fileContents, JsFinalizeCallback fileContentFinalizeCallback, JsValueRef *byteCodeBuffer);
+    static HRESULT CreateAndRunSerializedScript(const char* fileName, LPCSTR fileContents, size_t fileLength, JsFinalizeCallback fileContentsFinalizeCallback, char *fullPath);
+    static HRESULT CreateLibraryByteCodeHeader(LPCSTR contentsRaw, JsFinalizeCallback contentsRawFinalizeCallback, DWORD lengthBytes, LPCWSTR bcFullPath, LPCSTR libraryNameNarrow);
     static HRESULT RunScript(const char *fileName, LPCSTR fileContents, size_t fileLength,
                       JsFinalizeCallback fileContentsFinalizeCallback, JsValueRef bufferValue, char *fullPath,
                       JsValueRef parserStateCache);
+
+    static HRESULT GetParserStateBuffer(LPCSTR fileContents, JsFinalizeCallback fileContentsFinalizeCallback, JsValueRef *parserStateBuffer);
+    static HRESULT CreateParserState(LPCSTR fileContents, size_t fileLength, JsFinalizeCallback fileContentsFinalizeCallback, LPCWSTR fullPath);
 
     static JsRuntimeHandle chRuntime;
     static const size_t ttUriBufferLength = MAX_PATH * 3;

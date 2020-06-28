f73eeb6762fe2f4ec76c2ab2210338323f60c740
Maunder <maunderscott@gmail.com>
2020-06-28 13:24:12 +1200

    testing import functionality

---
+++ assets/scripts/module.js
@@ -0,0 +1,2 @@
+export const heh = () => console.log('module BB');
+export default () => console.log('im the default hehehehehheh')
---
+++ assets/scripts/test.js
@@ -0,0 +1,9 @@
+// YOU MUST DEFINE THE FILE EXTENSION
+import demoDefault, {heh} from "module.js"
+
+demoDefault();
+heh();
+
+console.log("Jaaaaaaaa")
+
+setTimeout(() => console.log("timeout is working"), 3000)

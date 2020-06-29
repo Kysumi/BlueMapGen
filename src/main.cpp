65c1908a0aeb77ce06c4c3b1dcbe718134e68153
Maunder <maunderscott@gmail.com>
2020-06-29 17:21:57 +1200

    Removing window loop as JS will manage it

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <SFML/Graphics.hpp>
 #include <src/Binding/Grid.h>
 #include "ChakraCore/Chakra.h"
+#include "WindowManager.h"
 
 int main()
 {
@@ -15,30 +16,9 @@ int main()
 
     binding::Grid::bind();
 
-    // create the window
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
-    // run the program as long as the window is open
-    while (window.isOpen())
-    {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        // clear the window with black color
-        window.clear(sf::Color::Black);
-
-        // draw everything here...
-        // window.draw(...);
-
-        // end the current frame
-        window.display();
-    }
+    WindowManager::setActiveWindow(&window);
 
     return 0;
 }

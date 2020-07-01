621a9822437a731719b0a2f0f6c587002fab3ead
Maunder <maunderscott@gmail.com>
2020-07-01 19:04:12 +1200

    Started testing vector

--- assets/scripts/test.js
+++ assets/scripts/test.js
@@ -3,29 +3,27 @@
 
 let grid = new Grid(20,20);
 
-Window.describe()
 
-let x = 0;
-let y = 0;
+
+const proccess = () => {
+
+    let x = 0;
+    let y = 0;
+
+    let node = grid.getNodeFromGridPosition(x,y);
+}
 
 while(Window.isOpen()) {
 
-    Window.processEvents();
-
+    // Window.processEvents();
+    console.log(grid.getSize().x())
     Window.clear();
-    let node = grid.getNodeFromGridPosition(x,y);
-    node.draw();
 
-    grid.process();
-    // grid.draw();
+    // node.draw();
+
+    // grid.process();
+    grid.draw();
 
     Window.display();
 
-    x++;
-    y++;
-
-    if (x > 10 || y > 10) {
-        x = 0;
-        y = 0;
-    }
 }

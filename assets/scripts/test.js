de61ca4398418d27464e29834c1a4aac829026e1
Maunder <maunderscott@gmail.com>
2020-07-07 12:17:52 +1200

    cleaning up the JS

--- assets/scripts/test.js
+++ assets/scripts/test.js
@@ -11,7 +11,7 @@ const proccess = () => {
     for (let x = 0; x < xSize; x++) {
         for (let y = 0; y < ySize; y++) {
             let node = grid.getNodeFromGridPosition(x,y);
-
+            node.draw();
         }
     }
 }
@@ -19,15 +19,9 @@ const proccess = () => {
 while(Window.isOpen()) {
 
     // Window.processEvents();
-    console.log(grid.getSize().x())
     Window.clear();
 
-    proccess()
-
-    // node.draw();
-
-    // grid.process();
-    grid.draw();
+    proccess();
 
     Window.display();
 

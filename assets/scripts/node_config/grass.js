0b77c6278ef0c42081ca4188234da001d0188fab
Maunder <maunderscott@gmail.com>
2020-07-18 16:32:08 +1200

    Now flickers

--- assets/scripts/node_config/grass.js
+++ assets/scripts/node_config/grass.js
@@ -1,3 +1,23 @@
+import {getNodesOfType} from "../node.js";
+import {randomTexture} from "./configs.js";
+import dirtTextures from "tiles_textures/dirt.js";
+import waterTextures from "tiles_textures/river.js";
+
 export const processGrass = (node, grid) => {
+    let neighbours = grid.getNeighbours(node.x(), node.y())
+
+    const neighboursOfType = getNodesOfType(neighbours, node.getBiome())
+    const typeCount = neighboursOfType.length
+
+    if(typeCount < 2)
+    {
+        node.setTexture(randomTexture(waterTextures))
+        node.setBiome("river")
+    }
+    else if(typeCount > 3)
+    {
+        node.setTexture(randomTexture(dirtTextures))
+        node.setBiome("dirt")
+    }
     console.log('Grass!!')
 }
--- assets/scripts/node_config/river.js
+++ assets/scripts/node_config/river.js
@@ -1,3 +1,18 @@
+import {getNodesOfType} from "../node.js";
+import {randomTexture} from "./configs.js";
+import dirtTextures from "tiles_textures/dirt.js";
+
 export const processRiver = (node, grid) => {
-    console.log('twas river!!')
+    let neighbours = grid.getNeighbours(node.x(), node.y())
+
+    const neighboursOfType = getNodesOfType(neighbours, node.getBiome())
+    const typeCount = neighboursOfType.length
+
+    if(typeCount > 3)
+    {
+        node.setTexture(randomTexture(dirtTextures))
+        node.setBiome("dirt")
+    }
+
+    console.log('Heh river go brrrr')
 }
